7e5ebaf48035612f8bd1452bff0dd38cea460c2e
Peter Simons <simons@cryp.to>
2013-04-18 13:22:33 +0200

    release.nix: add builds for hledger-lib, hledger-web, and hledger

--- release.nix
+++ release.nix
@@ -1,36 +1,77 @@
 /* Build instructions for the continuous integration system Hydra. */
 
-{ src ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
-, hledgerSrc ? { outPath = ../hledger; revCount = 0; gitTag = "dirty"; }
+{ hledgerSrc ? { outPath = ../hledger; revCount = 0; gitTag = "dirty"; }
+, hledgerInterestSrc ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
 }:
 
 let
-  version = src.gitTag;
   genAttrs = (import <nixpkgs> { }).lib.genAttrs;
+  supportedCompilers = [ "ghc704" "ghc742" "ghc762" ];
+  supportedPlatforms = [ "x86_64-linux" ];
 in
 rec {
-  build = genAttrs [ "ghc742" "ghc762" ] (ghcVer: genAttrs [ "x86_64-linux" ] (system:
+  hledgerLib = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
     let
       pkgs = import <nixpkgs> { inherit system; };
       haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
     in
-    pkgs.releaseTools.nixBuild {
-      name = "hledger-interest-${version}";
-      src = src;
-      buildInputs = with haskellPackages; [
-        ghc cabalDev cabalInstall
-        cmdargs csv mtl parsec prettyShow regexCompat regexpr safe split
-        time transformers utf8String HUnit
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hledger-lib";
+      src = hledgerSrc;
+      version = hledgerSrc.gitTag;
+      postUnpack = "sourceRoot+=/hledger-lib";
+      buildDepends = with haskellPackages; [
+        cmdargs csv filepath HUnit mtl parsec prettyShow regexCompat
+        regexpr safe split time transformers utf8String
       ];
-      configurePhase = ''
-        cp -r ${hledgerSrc}/hledger-lib/ hledger-lib/
-        chmod -R u+rw hledger-lib/
-        cabal-dev add-source hledger-lib/
-        cabal-dev install-deps
-        cabal-dev configure -v
-      '';
-      buildPhase = "cabal-dev build";
-      installPhase = "cabal-dev install";
-      checkPhase = "cabal-dev test";
-    }));
+    })));
+
+  hledger = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      myHledgerLib = pkgs.lib.getAttrFromPath [ghcVer system] hledgerLib;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hledger";
+      src = hledgerSrc;
+      version = hledgerSrc.gitTag;
+      postUnpack = "sourceRoot+=/hledger";
+      buildDepends = with haskellPackages; [ myHledgerLib haskeline shakespeareText ];
+    })));
+
+  hledgerWeb = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      myHledgerLib = pkgs.lib.getAttrFromPath [ghcVer system] hledgerLib;
+      myHledger = pkgs.lib.getAttrFromPath [ghcVer system] hledger;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hledger-web";
+      src = hledgerSrc;
+      version = hledgerSrc.gitTag;
+      postUnpack = "sourceRoot+=/hledger-web";
+      buildDepends = with haskellPackages; [
+        blazeHtml blazeMarkup clientsession cmdargs dataDefault filepath
+        hamlet hjsmin myHledger myHledgerLib httpConduit HUnit monadControl
+        networkConduit parsec regexpr safe shakespeareCss shakespeareJs
+        shakespeareText text time transformers wai waiExtra warp yaml yesod
+        yesodCore yesodDefault yesodForm yesodStatic blazeHtml
+      ];
+      testDepends = with haskellPackages; [ yesodCore yesodDefault yesodTest ];
+    })));
+
+  hledgerInterest = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      myHledgerLib = pkgs.lib.getAttrFromPath [ghcVer system] hledgerLib;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hledger-interest";
+      src = hledgerInterestSrc;
+      version = hledgerInterestSrc.gitTag;
+      buildDepends = with haskellPackages; [ myHledgerLib mtl ];
+    })));
 }
