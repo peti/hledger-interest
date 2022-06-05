8258b51037e398ecb29bee89c5acf383dd5d2d8f
Jack Todaro <solpeth@posteo.org>
2022-06-05 22:10:06 +1000

    Main.hs: adapt to hledger-lib-1.26
    Signed-off-by: Jack Todaro <solpeth@posteo.org>

--- Main.hs
+++ Main.hs
@@ -92,7 +92,7 @@ main = bracket (return ()) (\() -> hFlush stdout >> hFlush stderr) $ \() -> do
   when (isNothing (optDCC opts)) (commandLineError "no day counting convention specified\n")
   when (isNothing (optRate opts)) (commandLineError "no interest rate specified\n")
   let ledgerInputOptions = definputopts { balancingopts_ = (balancingopts_ definputopts) { ignore_assertions_ = optIgnoreAssertions opts } }
-  jnl' <- readJournalFiles ledgerInputOptions (reverse (optInput opts)) >>= either fail return
+  jnl' <- (runExceptT $ readJournalFiles ledgerInputOptions (reverse (optInput opts))) >>= either fail return
   interestAcc <- case args of
                    []    -> commandLineError "required argument ACCOUNT is missing\n"
                    [acc] -> return $ T.pack acc
